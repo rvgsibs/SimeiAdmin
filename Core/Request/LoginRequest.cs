81401a36eb2c98c3fe6875721aa8d5a41eaa7c46
Rodrigo Sousa <43799288+rvgsibs@users.noreply.github.com>
2024-06-26 11:21:16 -0300

    Initial commit

---
+++ Core/Request/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TemplateBlazorAdmin.Core.Request;
+
+public class LoginRequest
+{
+    public string Usuario { get; set; } = null!;
+    public string Senha { get; set; } = null!;
+
+}
---
+++ Core/Services/LoginService.cs
@@ -0,0 +1,29 @@
+﻿using TemplateBlazorAdmin.Core.Request;
+using TemplateBlazorAdmin.Core.Response;
+using TemplateBlazorAdmin.Core.Services.Interface;
+
+namespace TemplateBlazorAdmin.Core.Services;
+
+public class LoginService : ILoginService
+{
+    public LoginService()
+    {
+
+    }
+    public async Task<LoginResponse>? Login(LoginRequest loginRequest)
+    {
+        if (loginRequest.Usuario == "admin" && loginRequest.Senha == "123")
+        {
+            var retorno = new LoginResponse
+            {
+                Error = "",
+                Expiration = DateTime.Now.AddMinutes(15).ToString(),
+                NomeUsuario = loginRequest.Usuario,
+                Token = "1234567890asdfghjklpoiuytrewqmnbvcxz09876543210"
+            };
+
+            return retorno;
+        }
+        return null;
+    }
+}
